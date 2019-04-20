492c50e4f240b37fbdbe836c981df2086c3b0698
igorzakhar <igorzakhar@gmail.com>
2019-04-20 21:15:30 +0300

    Step 2-5: Attach a gun to the spaceship

--- space.py
+++ space.py
@@ -78,23 +78,29 @@ async def animate_spaceship(canvas, frames):
         await sleep(0.3)
 
 
-async def run_spaceship(canvas, start_row, start_column):
+async def run_spaceship(canvas, coros, start_row, start_column):
     height, width = canvas.getmaxyx()
-    border_size = 1
-
+    border_size = symbol_size = 1
     frame_size_y, frame_size_x = get_frame_size(spaceship_frame)
     frame_pos_x = round(start_column) - round(frame_size_x / 2)
     frame_pos_y = round(start_row) - round(frame_size_y / 2)
 
-    row_speed = column_speed = 0
+    row_speed, column_speed = 0, 0
 
     while True:
+
         draw_frame(canvas, frame_pos_y, frame_pos_x,
                    spaceship_frame, negative=True)
 
-        await sleep(0.3)
+        await sleep(0.1)
 
-        direction_y, direction_x, _ = read_controls(canvas)
+        direction_y, direction_x, spacebar = read_controls(canvas)
+
+        if spacebar:
+            shot_pos_x = frame_pos_x + round(frame_size_x / 2)
+            shot_pos_y = frame_pos_y - symbol_size
+            shot_coro = fire(canvas, shot_pos_y, shot_pos_x)
+            coros.append(shot_coro)
 
         row_speed, column_speed = update_speed(
             row_speed,
@@ -114,10 +120,13 @@ async def run_spaceship(canvas, start_row, start_column):
 
         frame_pos_x = min(frame_x_max, field_x_max) - frame_size_x
         frame_pos_y = min(frame_y_max, field_y_max) - frame_size_y
+
         frame_pos_x = max(frame_pos_x, border_size)
         frame_pos_y = max(frame_pos_y, border_size)
 
-        draw_frame(canvas, frame_pos_y, frame_pos_x, spaceship_frame)
+        draw_frame(canvas, frame_pos_y, frame_pos_x,
+                   spaceship_frame)
+
         canvas.refresh()
 
 
@@ -167,23 +176,24 @@ def main(canvas):
         for row, column, symbol in stars_generator(height, width)
     ]
 
-    start_row = height - 2
-    start_col = width / 2
-    coro_shot = fire(canvas, start_row, start_col)
-    coroutines.append(coro_shot)
-
     garbage_frames = get_frames_list(GARBAGE_FRAMES_DIR)
     garbage_coro = fill_orbit_with_garbage(canvas, coroutines, garbage_frames)
 
     rocket_frames = get_frames_list(ROCKET_FRAMES_DIR)
     start_rocket_row = height / 2
+    start_rocket_col = width / 2
 
     rocket_anim_coro = animate_spaceship(canvas, rocket_frames)
-    rocket_control_coro = run_spaceship(canvas, start_rocket_row, start_col)
+    rocket_control_coro = run_spaceship(
+        canvas,
+        coroutines,
+        start_rocket_row,
+        start_rocket_col
+    )
 
-    coroutines.append(garbage_coro)
     coroutines.append(rocket_anim_coro)
     coroutines.append(rocket_control_coro)
+    coroutines.append(garbage_coro)
 
     canvas.refresh()
 
